c2811c026d1be25c6daa0cf0ff148d69a0ce547e
fkgrkyr <fkgrkyr@gmail.com>
2025-10-29 02:34:39 +0900

    #1 [CHORE] 라이브러리 설치 및 ReactQueryProvider 유틸 함수 작성

---
+++ src/utils/ReactQueryProvider.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: 1,
+    },
+  },
+});
+
+export default function ReactQueryProvider({
+  children,
+}: React.PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
