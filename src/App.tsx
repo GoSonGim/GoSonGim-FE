ab76e713102d04b3bd7165851bae23910d909df9
fkgrkyr <fkgrkyr@gmail.com>
2025-11-04 20:51:17 +0900

    #10 [FEAT] 로그인 필요한 페이지 router로 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from '@/components/layout/Layout';
-import Home from '@/pages/home/Home';
-import Login from '@/pages/login/Login';
-import LoginForm from '@/pages/loginForm/LoginForm';
-import SignupForm from '@/pages/signupForm/SignupForm';
-import NicknamePage from '@/pages/nickname/NicknamePage';
-import GoogleCallback from '@/pages/auth/GoogleCallback';
-import Search from '@/pages/search/Search';
-import Review from '@/pages/review/Review';
-import Profile from '@/pages/profile/Profile';
+import { RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* 구글 OAuth Callback - Layout 없이 처리 */}
-        <Route path="/callback" element={<GoogleCallback />} />
+import { router } from './router';
 
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login/email" element={<LoginForm />} />
-          <Route path="/signup/email" element={<SignupForm />} />
-          <Route path="/signup/nickname" element={<NicknamePage />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
