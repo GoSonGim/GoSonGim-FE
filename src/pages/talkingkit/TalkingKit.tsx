a2b717d4f6d95591def610693c7401e62126f672
fkgrkyr <fkgrkyr@gmail.com>
2025-11-08 04:04:41 +0900

    #12 [REFACTOR] mock → mocks 폴더명 변경

--- src/pages/talkingkit/TalkingKit.tsx
+++ src/pages/talkingkit/TalkingKit.tsx
@@ -1,6 +1,6 @@
 import LeftArrowIcon from '@/assets/svgs/talkingkit/common/leftarrow.svg';
 import KitCard from '@/components/talkingkit/common/KitCard';
-import { kitsData } from '@/mock/talkingkit/kitsData';
+import { kitsData } from '@/mocks/talkingkit/kitsData';
 
 const TalkingKit = () => {
   return (
