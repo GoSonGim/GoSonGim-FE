e67107db2d113442e28f7aac5c3246d86a56a83e
fkgrkyr <fkgrkyr@gmail.com>
2025-11-24 04:46:43 +0900

    #46 [FIX] vite config host 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,12 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import svgr from 'vite-plugin-svgr';
-// import basicSsl from '@vitejs/plugin-basic-ssl'; // iOS 경고로 인해 임시 비활성화
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    // basicSsl(), // HTTPS 개발 서버를 위한 자체 서명 인증서 생성 - iOS 경고로 인해 임시 비활성화
     svgr({
       include: '**/*.svg', // ?react 안붙혀도 컴포넌트로 인식
       svgrOptions: {
@@ -21,7 +19,6 @@ export default defineConfig({
   ],
   server: {
     port: 8080,
-    host: true,
     // 개발 환경에서는 프록시 사용, 프로덕션에서는 실제 URL 사용
     proxy: {
       '/api': {
@@ -30,7 +27,6 @@ export default defineConfig({
         secure: true,
         // 대용량 파일 업로드를 위한 타임아웃 설정 (60초)
         timeout: 60000,
-        // ws: true, // WebSocket 지원 (필요시)
         // 프록시 요청 전처리
         configure: (proxy) => {
           proxy.on('proxyReq', (proxyReq, req) => {
